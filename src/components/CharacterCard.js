bd253b9065fc4174f60ee98276c60fd27023e527
Maddy <mmcintosh2k14@gmail.com>
2019-10-18 11:08:14 -0400

    adds completed character list and character card components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  const { name, status, species, gender } = props.character;
+  return <span className="character-card">
+          <h2>{name}</h2> 
+          <h3>{status}</h3>
+          <h3>{species}</h3>
+          <h3>{gender}</h3>
+
+        </span>;
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+    const getCharacters = () => {
+      axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.error('Oops, no data found here!', error);
+      });
+    }
+    getCharacters();
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map() over your state here!</h2> */}
+      {characters.map(character => (
+        <CharacterCard key={character.id} character={character} />
+      ))}
     </section>
   );
 }
