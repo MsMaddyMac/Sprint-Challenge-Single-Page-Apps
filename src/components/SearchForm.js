a3accd63f5f2685b743d8fdc34b99d82e7cb83d1
Maddy <mmcintosh2k14@gmail.com>
2019-10-18 12:57:33 -0400

    adds beginning of search component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SearchForm() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('')
+    const results = characters.filter(character =>
+      character.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResult(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
  
   return (
     <section className="search-form">
-     // Add a search form here
+     
     </section>
   );
 }
