fecba39c16c3779b615c660355d44287f2885e23
Maddy <mmcintosh2k14@gmail.com>
2019-10-18 11:31:27 -0400

    adds router and links welcome and character pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route, Link } from "react-router-dom";
 import CharacterList from "./components/CharacterList";
-
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/characters">Characters</Link>
+      </nav>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 export default function CharacterCard(props) {
-  const { name, status, species, gender } = props.character;
+  const { name, status, species, gender, image } = props.character;
   return <span className="character-card">
+          <img className="character-image" alt="rick and morty character" src={image} />
           <h2>{name}</h2> 
-          <h3>{status}</h3>
-          <h3>{species}</h3>
-          <h3>{gender}</h3>
-
+          <h3>Status: {status}</h3>
+          <h3>Species: {species}</h3>
+          <h3>Gender: {gender}</h3>
         </span>;
 }
